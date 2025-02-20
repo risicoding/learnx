ca0072c8c578f4f6c499d17e3b9625a76f0edd85
risik34 <risimn7@gmail.com>
2025-02-20 11:09:00 +0000

    feat: add pdf form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className='antialiased'
+        className=' min-h-screen antialiased'
       >
         {children}
       </body>
